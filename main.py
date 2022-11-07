61eba94cfc79a27d21b3f87604911450d135d377
thohid44 <thohidislam44@gmail.com>
2022-11-07 23:28:45 +0600

    First code of python

---
+++ main.py
@@ -0,0 +1,27 @@
+
+float_num = 5.4
+
+str_var = "SATT"
+
+int_num2, float_num2, str_var2 = 10, 5.4, "SATT"
+
+print(int_num2)
+
+#Assign same value different variable in python
+x=y=z = "Bangladesh"
+# Check the variable type class
+
+print(x, "is of type",type(x))
+
+num = 1+2j
+print(num, "is complex number?", isinstance(1+2j,complex))
+
+a = 1234567890123456789
+print(a)
+
+b = 0.1234567890123456789
+print(b)
+c = 1+2j
+print(c)
+
+#
